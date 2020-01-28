366e7e8a109cc87f8f1de0f0ad486e112645bf4a
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-28 22:47:55 +0000

    add back original server.py

--- google-app-engine/app/server.py
+++ google-app-engine/app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
 model_file_name = 'model'
-classes = ['rowing_eight', 'sculling_double', 'sculling_quad','sculling_single']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
